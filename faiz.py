e432e942dccbe2f1addfd73c2b9ff6b2471620d6
FAIZ-OFFICIALS <pubgytab2@gmail.com>
2025-01-26 18:32:58 +0500

    Add files via upload

---
+++ faiz.py
@@ -0,0 +1,11 @@
+import os,platform
+os.system('git pull --quiet 2>/dev/null')
+os.system("clear")
+print('\x1b[38;5;216m [\x1b[38;5;161m•\x1b[38;5;216m] \x1b[38;5;188mJoin Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/DVq6v6lasB5LfIZb4D1NNl')
+faiz=platform.architecture()[0]
+if faiz=="32bit":
+    os.system("clear")
+    exit('\x1b[38;5;216m [\x1b[38;5;161m•\x1b[38;5;216m] \033[91;1mSorry Bro 32 Bit Devices Not Supported')
+elif faiz=="64bit":
+    __import__("p64")
