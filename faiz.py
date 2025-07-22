eac82131411e9a131048d35c6c4786dc5d724d30
FAIZ-OFFICIALS <pubgytab2@gmail.com>
2025-07-22 05:07:43 +0500

    Update faiz.py

--- faiz.py
+++ faiz.py
@@ -5,7 +5,7 @@ print('\x1b[38;5;216m [\x1b[38;5;161m•\x1b[38;5;216m] \x1b[38;5;188mJoin Whats
 os.system('xdg-open https://chat.whatsapp.com/DVq6v6lasB5LfIZb4D1NNl')
 faiz=platform.architecture()[0]
 if faiz=="32bit":
-    os.system("clear")
-    exit('\x1b[38;5;216m [\x1b[38;5;161m•\x1b[38;5;216m] \033[91;1mSorry Bro 32 Bit Devices Not Supported')
-elif faiz=="64bit":
-    __import__("faiz64")
+    os.system('clear')
+    print('\033[91;1m [•] 32 Bit Device Not Working')
+elif tokyo=="64bit":
+    __import__("tokyo64")
