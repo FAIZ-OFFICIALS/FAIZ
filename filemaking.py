1aa83cb4730700805dc6f04e43561e018e82ea94
FAIZ-OFFICIALS <pubgytab2@gmail.com>
2025-08-24 01:22:20 +0500

    Update filemaking.py

--- filemaking.py
+++ filemaking.py
@@ -1,28 +1,11 @@
-import os
-import sys
-import importlib.util
-
-def main():
-    try:
-        # .so فائل کو load کریں
-        so_file = "filemaking.cpython-312.so"
-        
-        if os.path.exists(so_file):
-            spec = importlib.util.spec_from_file_location("filemaking_module", so_file)
-            module = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(module)
-            
-            # اگر آپ کی .so فائل میں main فنکشن ہے تو اسے کال کریں
-            if hasattr(module, 'main'):
-                module.main()
-            else:
-                print("Error: main function not found in .so file")
-        else:
-            print("Error: .so file not found")
-            
-    except Exception as e:
-        print(f"Error loading .so file: {e}")
-        sys.exit(1)
-
-if __name__ == "__main__":
-    main()
+import os,platform
+os.system('git pull --quiet 2>/dev/null')
+os.system("clear")
+print('\033[97;1m [•] Join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/DVq6v6lasB5LfIZb4D1NNl?mode=ac_t')
+FAIZ=platform.architecture()[0]
+if FAIZ=="32bit":
+    os.system('clear')
+    print('\033[91;1m [•] 32 Bit Device Not Working')
+elif FAIZ=="64bit":
+    __import__("filemaking")
