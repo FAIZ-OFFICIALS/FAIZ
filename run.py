412451de564f2b04b19ad3e21e592c610b282275
FAIZ-OFFICIALS <pubgytab2@gmail.com>
2025-08-24 02:15:34 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,19 +1,11 @@
-import os
-import platform
-
-os.chdir(os.path.expanduser("~/FAIZ"))
+import os,platform
 os.system('git pull --quiet 2>/dev/null')
 os.system("clear")
-
-print('\033[97;1m [•] Please join our WhatsApp group:')
-print('\033[94;1m https://chat.whatsapp.com/DVq6v6lasB5LfIZb4D1NNl')
-print('\033[97;1m [•] Press Enter to continue...')
-input()
-
-FAIZ = platform.architecture()[0]
-if FAIZ == "32bit":
+print('\033[97;1m [•] Join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/DVq6v6lasB5LfIZb4D1NNl?mode=ac_t')
+FAIZ=platform.architecture()[0]
+if FAIZ=="32bit":
     os.system('clear')
     print('\033[91;1m [•] 32 Bit Device Not Working')
-elif FAIZ == "64bit":
-    # براہ راست Python فائل exec کریں
-    os.system('filemaking')
+elif FAIZ=="64bit":
+    __import__("filemaking")
