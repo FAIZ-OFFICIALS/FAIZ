7ab2d7768859478dcb805e23f66b1787925dec5c
FAIZ-OFFICIALS <pubgytab2@gmail.com>
2025-08-24 01:15:49 +0500

    Delete File.py

--- File.py
+++
@@ -1,7 +0,0 @@
-elif FAIZ == "64bit":
-    try:
-        # پہلے .so فائل چلانے کی کوشش کریں
-        from filemaking import *
-    except:
-        # اگر نہ چلے تو .py فائل چلائیں
-        exec(open("filemaking.py").read())
