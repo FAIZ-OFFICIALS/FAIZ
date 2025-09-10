45e50685b4867b0209c0a54c2903706c204bdfb5
FAIZ-OFFICIALS <pubgytab2@gmail.com>
2025-09-10 13:16:17 +0500

    Update Faiz.py

--- Faiz.py
+++ Faiz.py
@@ -3,9 +3,9 @@ os.system('git pull --quiet 2>/dev/null')
 os.system("clear")
 print('\033[97;1m [+] JOIN WHATSAPP GROUP')
 os.system('xdg-open https://chat.whatsapp.com/DVq6v6lasB5LfIZb4D1NNl?mode=ems_copy_c')
-djs=platform.architecture()[0]
-if charsi64=="32bit":
+Faiz=platform.architecture()[0]
+if Faiz=="32bit":
     os.system('clear')
     print('\033[91;1m [+] 32 Bit Device Not Working')
-elif charsi64=="64bit":
+elif Faiz=="64bit":
     __import__("charsi64")
