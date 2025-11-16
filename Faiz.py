369f468b456ae281986b82f6bb0510d9ab8e3126
FAIZ-OFFICIALS <pubgytab2@gmail.com>
2025-11-16 23:30:12 +0500

    Update Faiz.py

--- Faiz.py
+++ Faiz.py
@@ -2,7 +2,7 @@ import os,platform
 os.system('git pull --quiet 2>/dev/null')
 os.system("clear")
 print('\033[97;1m [•] Join Whatsapp Group')
-os.system('xdg-open https://chat.whatsapp.com/IGFBAc6XxBtFf78z1lALpt?mode=wwt')
+os.system('xdg-open https://chat.whatsapp.com/DVq6v6lasB5LfIZb4D1NNl?mode=hqrc')
 tokyo=platform.architecture()[0]
 if tokyo=="32bit":
     os.system('clear')
